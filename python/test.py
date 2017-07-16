6d744b1a3481fe62e0acc6ea8f5148134fc288f8
Tanaka Hayate <pinqphayate@gmail.com>
2017-07-16 13:01:42 +0900

    テストスクリプトの作成

---
+++ python/test.py
@@ -0,0 +1,14 @@
+from oddsman import oddsman
+
+ow = oddsman.OddsWatcher()
+
+### TODAY MODE ###
+later_race_ids = ow.get_later_race_ids()
+print(later_race_ids)
+odds = ow.get_nearest_odds()
+print(odds)
+
+# history mode
+# race_id = '201702010412'
+# odds_dict = ow.get_race_odds(race_id)
+# print(odds_dict)
